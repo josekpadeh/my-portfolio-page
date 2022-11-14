bc4694bbedb5f478ab6641776185a35eb5ebb32f
I. Jose Kpadeh <ijosekpadeh@gmail.com>
2022-11-14 16:10:15 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import AuthorCard from './components/AuthorCard';
+import Work from './components/Work';
+import Education from './components/Education';
+import Hobbies from './components/Hobbies';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <AuthorCard/>
+      <Work/>
+      <Education/>
+      <Hobbies/>
+      
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Education.js
@@ -0,0 +1,16 @@
+import React from "react";
+export default function Education(){
+    return(
+        <div id="education" class="bg-[#
+            3EAEAEA] h-50 my-6 py-8 rounded-lg border-4">
+
+<div class="flex justify-center">
+                <h3 class="border-4 w-20  border-dashed px-4 py-4">Education</h3>
+              </div>
+
+
+            </div>
+
+            
+    )
+}
